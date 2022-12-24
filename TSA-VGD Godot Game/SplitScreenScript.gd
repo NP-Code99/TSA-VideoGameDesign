bba8e66ebeb8f12de92c1feecbafcf8b80b25d0e
Nandan Pullakandam <nandan.pullakandam12@gmail.com>
2022-12-24 16:06:16 -0500

    Creation of Majority of the Lobby Map

---
+++ TSA-VGD Godot Game/SplitScreenScript.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+
+onready var players := { 
+	"1" : {
+		viewport = $"HBoxContainer/ViewportContainer/Viewport",
+		camera = $"HBoxContainer/ViewportContainer/Viewport/Camera2D",
+		player = $HBoxContainer/ViewportContainer/Viewport/Node2D/LobbyPlayer1,
+	},
+	"2" : {
+		viewport = $"HBoxContainer/ViewportContainer2/Viewport",
+		camera = $"HBoxContainer/ViewportContainer2/Viewport/Camera2D",
+		player = $HBoxContainer/ViewportContainer/Viewport/Node2D/LobbyPlayer2,
+		
+	}
+		
+}
+
+func _ready() -> void:
+	players["2"].viewport.world_2d = players["1"].viewport.world_2d
+	for node in players.values():
+		var remote_transform := RemoteTransform2D.new()
+		remote_transform.remote_path = node.camera.get_path()
+		node.player.add_child(remote_transform)
