e52dafc5dedcfeba72a735adef59577b83deeb7c
anikash1232 <anirudh.vpk@gmail.com>
2023-01-28 20:33:34 -0500

    Finished Minigame 3 Movement

--- TSA-VGD Godot Game/Actors/Player.gd
+++ TSA-VGD Godot Game/Actors/Player.gd
@@ -1,32 +1,51 @@
-extends Actor
+extends KinematicBody2D
 
-func _physics_process(delta: float) -> void:
-	var is_jump_interrupted: = Input.is_action_just_released("jump") and velocity.y < 0.0
-	var direction: = get_direction()
-	velocity = calculate_move_velocity(velocity, direction, speed, is_jump_interrupted)
-	velocity = move_and_slide(velocity, FLOOR_NORMAL)
+const UP_DIRECTION := Vector2.UP
 
-func get_direction() -> Vector2:
-	return Vector2(
-		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-1.0 if Input.is_action_just_pressed("move_up") and is_on_floor() else 1.0
+export var speed := 600.0 
+export var jump_strength := 1500.0
+export var maximum_jumps := 2 
+export var double_jump_strength := 1200.0 
+export var gravity := 4500.0 
+
+var _jumps_made := 0
+var _velocity := Vector2.ZERO
+
+func _physics_process(_delta: float) -> void:
+	var _horizontal_direction = (
+		Input.get_action_strength("move_right")
+		- Input.get_action_strength("move_left")
 	)
+	_velocity.x = _horizontal_direction * speed
+	_velocity.y += gravity * _delta
+	
+	var is_falling := _velocity.y > 0.0 and not is_on_floor()
+	var is_jumping := Input.is_action_just_pressed("move_up") and is_on_floor()
+	var is_double_jumping := Input.is_action_just_pressed("move_up") and is_falling
+	var is_jump_cancelled := Input.is_action_just_released("move_up") and _velocity.y < 0.0
+	var is_idling := is_on_floor() and is_zero_approx(_velocity.x)
+	var is_running := is_on_floor() and not is_zero_approx(_velocity.x)
+	
+	if is_jumping: 
+		_jumps_made += 1
+		_velocity.y = -jump_strength
+	elif is_double_jumping:
+		_jumps_made += 1
+		if _jumps_made <= maximum_jumps: 
+			_velocity.y = -double_jump_strength
+	elif is_jump_cancelled: 
+		_velocity.y = 0.0
+	elif is_idling or is_running:
+		_jumps_made = 0
+	
+	if Input.is_action_pressed("key_respawn1"):
+		get_node(".").position.x = 0
+		get_node(".").position.y = 0
+		
+	_velocity = move_and_slide(_velocity, UP_DIRECTION)
+	
 
 
-func calculate_move_velocity(
-		linear_velocity: Vector2,
-		direction: Vector2, 
-		speed: Vector2,
-		is_jump_interrupted: bool 
-	) -> Vector2:
-	var new_velocity = linear_velocity
-	new_velocity.x = speed.x * direction.x
-	new_velocity.y = gravity * get_physics_process_delta_time()
-	if direction.y == -1.0:
-		new_velocity.y = speed.y * direction.y
-	if is_jump_interrupted:
-		new_velocity.y = 0.0
-	return new_velocity
 	
 
 
