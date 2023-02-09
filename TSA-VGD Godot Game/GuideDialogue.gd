ad95ddb6b886192a5c36b1994d070d399fbae3b5
anikash1232 <anirudh.vpk@gmail.com>
2023-02-08 19:39:39 -0500

    Minigame 3 semi completed

--- TSA-VGD Godot Game/GuideDialogue.gd
+++ TSA-VGD Godot Game/GuideDialogue.gd
@@ -10,10 +10,10 @@ func use_dialogue():
 
 func _on_Area2D_body_entered(body: Node) -> void:
 	if body.name == "Sullivan" || body.name == "Stephanie":
-		get_parent().get_node("Dialogues").visible = true
+		get_parent().get_node("Dialogues").get_node("NinePatchRect").visible = true
 		use_dialogue()
 
 
 func _on_Area2D_body_exited(body: Node) -> void:
 	if body.name == "Sullivan" || body.name == "Stephanie":
-		get_parent().get_node("Dialogues").visible = false
+		get_parent().get_node("Dialogues").get_node("NinePatchRect").visible = false
